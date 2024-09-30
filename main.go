d8b9179b305beb6392d28a3a9cb59f0c7807fd6f
Viet Pham <vietpham2017@gmail.com>
2024-09-29 22:02:27 -0500

    set up

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println(intDivision(4,2))
+}
+
+func intDivision(numerator int, denominator int) int {
+	return numerator / denominator
+}
+
